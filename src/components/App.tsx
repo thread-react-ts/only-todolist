0128536a68f3170a6cd81afc110a9a15c0f151ff
dimasyudhana <dimas.yudhana@gmail.com>
2024-04-06 15:07:47 +0700

    init project

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="container">
+            <h1>10 days with React-TS</h1>
+        </div>
+    );
+}
+
+export default App;
